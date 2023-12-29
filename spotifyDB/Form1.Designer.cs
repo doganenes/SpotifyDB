c44c92a57a2648cc44036fc698a2dae786670264
BarisZirhli <61722105+BarisZirhli@users.noreply.github.com>
2023-12-29 18:18:14 +0300

    Add files via upload
    userid colununun fk oldugu tüm tablolarda adı silinip, tekrar user tablosunda bu sefer tipi serialbigint seklinde oluşturulduktan sonra  kendi  postgresql connectiona göre düzenlenip çalıştırıldıgında veri ekleyecektir.

---
+++ spotifyDB/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace spotifyDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.userName = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.disconnectBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(260, 504);
+            this.addBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(219, 112);
+            this.addBtn.TabIndex = 0;
+            this.addBtn.Text = "Add Data";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 147);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 265);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "email";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 375);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 21);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Create Date";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // userName
+            // 
+            this.userName.Location = new System.Drawing.Point(202, 34);
+            this.userName.Margin = new System.Windows.Forms.Padding(4);
+            this.userName.Name = "userName";
+            this.userName.Size = new System.Drawing.Size(147, 29);
+            this.userName.TabIndex = 5;
+            this.userName.TextChanged += new System.EventHandler(this.userName_TextChanged);
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(202, 254);
+            this.email.Margin = new System.Windows.Forms.Padding(4);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(147, 29);
+            this.email.TabIndex = 6;
+            this.email.TextChanged += new System.EventHandler(this.email_TextChanged);
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(202, 143);
+            this.password.Margin = new System.Windows.Forms.Padding(4);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(147, 29);
+            this.password.TabIndex = 7;
+            this.password.TextChanged += new System.EventHandler(this.password_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(202, 367);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(290, 29);
+            this.dateTimePicker1.TabIndex = 9;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Location = new System.Drawing.Point(412, 64);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(106, 56);
+            this.connectBtn.TabIndex = 10;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // disconnectBtn
+            // 
+            this.disconnectBtn.Location = new System.Drawing.Point(569, 64);
+            this.disconnectBtn.Name = "disconnectBtn";
+            this.disconnectBtn.Size = new System.Drawing.Size(103, 56);
+            this.disconnectBtn.TabIndex = 11;
+            this.disconnectBtn.Text = "Disconnect";
+            this.disconnectBtn.UseVisualStyleBackColor = true;
+            this.disconnectBtn.Click += new System.EventHandler(this.disconnectBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 668);
+            this.Controls.Add(this.disconnectBtn);
+            this.Controls.Add(this.connectBtn);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.userName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addBtn);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "UserForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button addBtn;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox userName;
+        private TextBox email;
+        private TextBox password;
+        private DateTimePicker dateTimePicker1;
+        private Button connectBtn;
+        private Button disconnectBtn;
+    }
+}
---
+++ spotifyDB/Form1.cs
@@ -0,0 +1,127 @@
+using Npgsql;
+using System.Data;
+using System.Linq.Expressions;
+
+namespace spotifyDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string connString = "Server=localhost; Port=5432; Username=postgres; Password=mitaka; Database=spotify;";
+        private NpgsqlConnection conn;
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            string username = userName.Text;
+            string pass = password.Text;
+            string usermail = email.Text;
+            DateTime date = dateTimePicker1.Value;
+
+            string insert = "INSERT INTO tbluser (username, password, email, creationdate) VALUES" +
+                            " (@username, @pass, @usermail, @date)";
+
+            using (NpgsqlCommand cmd = new NpgsqlCommand(insert, conn))
+            {
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@pass", pass);
+                cmd.Parameters.AddWithValue("@usermail", usermail);
+                cmd.Parameters.AddWithValue("@date", date);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("User data inserted successfully!");
+        }
+        private void connectBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn == null)
+                {
+                    conn = new NpgsqlConnection(connString);
+                }
+
+                if (conn.State != ConnectionState.Open)
+                {
+                    conn.Open();
+                    // MessageBox.Show("Connected");
+                }
+                else
+                {
+                    // MessageBox.Show("Connection is already open.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void disconnectBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn != null && conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                    //  MessageBox.Show("Disconnected");
+                }
+                else
+                {
+                    // MessageBox.Show("Connection is already closed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void email_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
+
