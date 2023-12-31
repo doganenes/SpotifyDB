bac7e19306f5d88c76335492d09ac94b19c2a7b9
Enes Doğan <enesdoganlive@hotmail.com>
2023-12-31 20:52:27 +0300

    Added spotifyDB directory

---
+++ spotifyDB/playlist.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace spotifyDB
+{
+    partial class playlist
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            userForm = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(104, 85);
+            label1.Name = "label1";
+            label1.Size = new Size(147, 28);
+            label1.TabIndex = 0;
+            label1.Text = "PlayList Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(104, 181);
+            label2.Name = "label2";
+            label2.Size = new Size(163, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Created By User";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(206, 367);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(126, 76);
+            button1.TabIndex = 3;
+            button1.Text = "Add Data";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(285, 75);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(114, 34);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox2.Location = new Point(285, 171);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(114, 34);
+            textBox2.TabIndex = 5;
+            // 
+            // userForm
+            // 
+            userForm.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            userForm.Location = new Point(206, 484);
+            userForm.Margin = new Padding(3, 4, 3, 4);
+            userForm.Name = "userForm";
+            userForm.Size = new Size(126, 51);
+            userForm.TabIndex = 7;
+            userForm.Text = "Main Form";
+            userForm.UseVisualStyleBackColor = true;
+            userForm.Click += userForm_Click;
+            // 
+            // playlist
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(530, 600);
+            Controls.Add(userForm);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "playlist";
+            Text = "playlist";
+            Load += playlist_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button userForm;
+    }
+}
